c3314513a6837564e613f8a553a5012148acf1d8
sumalatha <sumagaddipati@gmail.com>
2026-05-01 16:42:40 +0530

    fix db public url

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "mysql+pymysql://root:PgpdfsphSdQVusvFFGRbuVuxnNJDOBes@mysql.railway.internal:3306/railway"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL,
