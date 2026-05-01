97b4049b2247828ab62efe8d81d150eb37e6f097
sumalatha <sumagaddipati@gmail.com>
2026-05-01 16:46:51 +0530

    fix database url format

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise Exception("DATABASE_URL not set properly")
 
 engine = create_engine(
     DATABASE_URL,
